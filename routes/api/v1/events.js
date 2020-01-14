839224fbf27301b7e8383339a66404458044cf34
Leiya Kenney <leiya.kenney@gmail.com>
2020-01-14 12:35:46 -0700

    Create events endpoint

---
+++ routes/api/v1/events.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../../../knexfile')[environment];
+const database = require('knex')(configuration);
+
+const EventsHelper = require('../../../helpers/eventsHelper.js');
+const eventsHelper = new EventsHelper();
+
+router.get('/', async function (request, response) {
+  await eventsHelper.createEventsResponse()
+  .then((data) => {
+    response.status(200).json(data);
+  })
+  .catch((error) => {
+    return response.status(500).json({ error });
+  })
+})
+
+module.exports = router;
